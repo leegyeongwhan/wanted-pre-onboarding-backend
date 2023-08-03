3c1444b28a662deb2accb0ee063b5680ba7cee9a
leegyeongwhan <leekhy02@naver.com>
2023-08-03 18:45:56 +0900

    chore: 실제 db에 들어갈 데이터 스키마 예시 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,35 @@
+create table member
+(
+    member_id       bigint auto_increment primary key,
+    email           varchar(128) not null UNIQUE,
+    password_id     bigint       not null,
+    member_token_id bigint       not null,
+    constraint fk_password
+        foreign key (password_id) references password (password_id),
+    constraint fk_member_token
+        foreign key (member_token_id) references town (member_token_id)
+);
+
+create table password
+(
+    password_id     bigint auto_increment primary key,
+    hashed_password varchar(255) not null
+);
+
+create table member_token
+(
+    member_token_id bigint auto_increment primary key,
+    jwt_token       varchar(500)
+);
+
+create table board
+(
+    board_id     bigint auto_increment primary key,
+    title        varchar(100) not null,
+    content      text         not null,
+    created_date datetime     not null DEFAULT CURRENT_TIMESTAMP,
+    deleted      tinyint(1) not null default 0,
+    member_id    bingint not null,
+    constraint fk_member
+        foreign key (member_id) references member (member_id),
+);
