f2cca6c32c2cbf0a0e2acc07ccc04790a9fd864e
leegyeongwhan <leekhy02@naver.com>
2023-08-04 18:01:23 +0900

    chore: 스키마 엔티티 구조변경
    - 프로젝트 규모로 인해 password 유저로 추가

--- src/main/java/onboarding/repository/PasswordRepository.java
+++
@@ -1,7 +0,0 @@
-package onboarding.repository;
-
-import onboarding.domain.Password;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PasswordRepository extends JpaRepository<Password, Long> {
-}
