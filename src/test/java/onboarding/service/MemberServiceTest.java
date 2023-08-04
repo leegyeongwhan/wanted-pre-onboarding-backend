d3c4ab8227e156629b06036606db30ec6c373a02
leegyeongwhan <leekhy02@naver.com>
2023-08-04 13:34:02 +0900

    feat: 회원 서비스 회원가입 단위테스트
    - 회원가입 메서드 단위테스트 구현

---
+++ src/test/java/onboarding/service/MemberServiceTest.java
@@ -0,0 +1,60 @@
+package onboarding.service;
+
+import onboarding.domain.Member;
+import onboarding.domain.Password;
+import onboarding.dto.request.SignUpRequest;
+import onboarding.repository.MemberRepository;
+import onboarding.repository.PasswordRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private PasswordRepository passwordRepository;
+    @Mock
+    private BCryptPasswordEncoder encoder;
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    void test_signup() {
+        //given
+        SignUpRequest signUpRequest = new SignUpRequest("example123@naver.com", "password123");
+        Password password = new Password("encodedPassword");
+        Member member = new Member(signUpRequest.getEmail(), password);
+
+        Long fakeMemberId = 1L;
+        ReflectionTestUtils.setField(member, "id", fakeMemberId);
+
+        given(encoder.encode(signUpRequest.getPassword())).willReturn("encodedPassword");
+        given(passwordRepository.save(any())).willReturn(password);
+        given(memberRepository.save(any())).willReturn(member);
+        given(memberRepository.findById(fakeMemberId)).willReturn(Optional.of(member));
+        //when
+        Long newMemberId = memberService.signup(signUpRequest);
+
+        //then
+        Member findMember = memberRepository.findById(newMemberId).get();
+        Assertions.assertEquals(member.getId(), findMember.getId());
+        Assertions.assertEquals(member.getPassword(), findMember.getPassword());
+        Assertions.assertEquals(member.getEmail(), findMember.getEmail());
+    }
+}
