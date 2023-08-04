69b9a1602f432851fd5ca2b60425b81eb16175cf
leegyeongwhan <leekhy02@naver.com>
2023-08-04 17:23:49 +0900

    feat: controller테스트 추가 회원가입에 필요한 dto들 맞게끔 변경

--- src/main/java/onboarding/domain/Password.java
+++ src/main/java/onboarding/domain/Password.java
@@ -1,11 +1,13 @@
 package onboarding.domain;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Password {
 
--- src/main/java/onboarding/dto/request/LoginRequest.java
+++ src/main/java/onboarding/dto/request/LoginRequest.java
@@ -1,16 +1,13 @@
 package onboarding.dto.request;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-
 @Getter
-@Setter
-public class SignUpRequest {
+public class LoginRequest {
     @Email
     @NotNull(message = "Email cannot be null")
     private String email;
@@ -18,9 +15,4 @@ public class SignUpRequest {
     @NotNull(message = "Password cannot be null")
     @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
-
-    public SignUpRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
 }
